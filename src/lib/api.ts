1c6affe5c42dfed6a8cf37e1c79a484523e202a8
orlandomiguell29 <orlando.garciav@autonoma.edu.co>
2026-09-03 10:42:30 -0500

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://inventory-back-767768515623.europe-west1.run.app';
 
 interface LoginCredentials {
   email: string;
